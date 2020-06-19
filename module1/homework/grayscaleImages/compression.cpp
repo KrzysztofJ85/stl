74d7d20ae54fe9caca674559145bdb41f677a975
Lukasz <lksobczyk@wp.pl>
2020-06-19 20:27:28 +0200

    Copy files from branch module1

---
+++ module1/homework/grayscaleImages/compression.cpp
@@ -0,0 +1,65 @@
+#include "compression.hpp"
+
+#include <iostream>
+
+constexpr uint8_t ASCIIspace = 32;
+
+VectorPair compressGrayscale(const ArrayArray& data) {
+    VectorPair result;
+    result.reserve(width * height);
+
+    for (const auto& entry : data) {
+        uint8_t currValue = entry.front();
+        uint8_t currCount = 1;
+
+        for (uint8_t id = 1; id < width; id++) {
+            if (entry[id] == currValue) {
+                currCount++;
+            }
+            else {
+                result.emplace_back(std::make_pair(currValue, currCount));
+                currValue = entry[id];
+                currCount = 1;
+            }
+        }
+        result.emplace_back(std::make_pair(currValue, currCount));
+    }
+    result.shrink_to_fit();
+
+    return result;
+}
+
+ArrayArray decompressGrayscale(const VectorPair& data) {
+    ArrayArray result;
+
+    size_t colCount = 0;
+    uint8_t rowId = 0;
+    auto it = result[rowId].begin();
+
+    for (const auto& entry : data) {
+        auto it_back = std::next(it, entry.second);
+        std::fill(it, it_back, entry.first);
+        colCount += entry.second;
+        if (colCount < width) {
+            it = it_back;
+        } else if (++rowId < height) {
+            colCount = 0;
+            it = result[rowId].begin();
+        }
+    }
+
+    return result;
+}
+
+void printMap(const ArrayArray& mapToPrint) {
+    for (const auto& column : mapToPrint) {
+        for (const auto& rowValue : column) {
+            if (rowValue > ASCIIspace) {
+                std::cout << rowValue;
+            } else {
+                std::cout << ASCIIspace;
+            }
+        }
+        std::cout << '\n';
+    }
+}
---
+++ module1/homework/grayscaleImages/compression.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <array>
+#include <vector>
+
+namespace
+{
+constexpr size_t width = 32; // 240;
+
+constexpr size_t height = 32; // 160;
+
+using VectorPair = std::vector<std::pair<uint8_t, uint8_t> >;
+
+using ArrayArray = std::array<std::array<uint8_t, height>, width>;
+}
+
+VectorPair compressGrayscale(const ArrayArray&);
+
+ArrayArray decompressGrayscale(const VectorPair&);
+
+void printMap(const ArrayArray&);
